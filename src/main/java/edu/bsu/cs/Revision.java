5b91a2d4eecb82a8bcf8e3ea1ac8fda87e17774e
carra <allison.carr@bsu.edu>
2024-02-20 00:17:25 -0500

    Revision class JSON methods were simplified to 2 sources. AC

--- src/main/java/edu/bsu/cs/Revision.java
+++ src/main/java/edu/bsu/cs/Revision.java
@@ -1,44 +1,39 @@
 package edu.bsu.cs;
 
 import com.jayway.jsonpath.JsonPath;
-import org.json.JSONArray;
+import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 
-
 public class Revision {
 
-
     public String parse(String jsonData) throws IOException {
-        JSONArray names = (JSONArray) JsonPath.read(jsonData, "$..revisions..user");
+        JSONArray names = JsonPath.read(jsonData, "$..revisions..user");
         System.out.println("14 Most Recent Revisions:");
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 14 && i < names.size(); i++) {
             System.out.println((i + 1) + ". " + names.get(i).toString());
         }
         return jsonData;
     }
 
     public String parseTimestamps(String jsonData) throws IOException {
-        JSONArray timestamps = (JSONArray) JsonPath.read(jsonData, "$..revisions..timestamp");
+        JSONArray timestamps = JsonPath.read(jsonData, "$..revisions..timestamp");
         System.out.println("Time: ");
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 14 && i < timestamps.size(); i++) {
             System.out.println((i + 1) + ". " + timestamps.get(i).toString());
         }
         return jsonData;
     }
 
     public String parseRedirects(String jsonData) throws IOException {
-        JSONArray redirects = (JSONArray) JsonPath.read(jsonData, "$..redirects");
+        JSONArray redirects = JsonPath.read(jsonData, "$..redirects");
         if (!redirects.isEmpty()) {
-            JSONArray toArray = (JSONArray) JsonPath.read(jsonData, "$..redirects[0].to");
-            String redirectTo = "";
-            if (!toArray.isEmpty()) {
-                redirectTo = (String) toArray.get(0);
-            }
+            JSONArray toArray = JsonPath.read(jsonData, "$..redirects[0].to");
+            String redirectTo = toArray.isEmpty() ? "" : toArray.get(0).toString();
             System.out.println("Redirected to: " + redirectTo);
         } else {
             System.out.println("No redirects.");
         }
         return jsonData;
-        }
+    }
 }
--- src/test/java/edu/bsu/cs/RevisionTest.java
+++ src/test/java/edu/bsu/cs/RevisionTest.java
@@ -5,33 +5,30 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.LocalDateTime;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RevisionTest {
 
     @Test
     public void testNameParse() throws IOException {
         Revision parser = new Revision();
-        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-        String user = parser.parse(String.valueOf(testDataStream));
+        InputStream testDataStream = getClass().getResourceAsStream("/test.json");
+        String user = parser.parse(new String(testDataStream.readAllBytes()));
         Assertions.assertEquals("24.143.118.36", user);
-
-
     }
+
     @Test
-    public void testTimestamps() throws IOException{
+    public void testTimestamps() throws IOException {
         Revision parser = new Revision();
-        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-        String timestamps = parser.parseTimestamps(String.valueOf(testDataStream));
+        InputStream testDataStream = getClass().getResourceAsStream("/test.json");
+        String timestamps = parser.parseTimestamps(new String(testDataStream.readAllBytes()));
         Assertions.assertEquals("2024-02-04T18:19:33Z", timestamps);
     }
+
     @Test
-    public void testRedirects()throws IOException{
+    public void testRedirects() throws IOException {
         Revision parser = new Revision();
-        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-        String redirects = parser.parseRedirects(testDataStream.toString());
+        InputStream testDataStream = getClass().getResourceAsStream("/test.json");
+        String redirects = parser.parseRedirects(new String(testDataStream.readAllBytes()));
         Assertions.assertEquals("Frank Zappa", redirects);
     }
-
 }
