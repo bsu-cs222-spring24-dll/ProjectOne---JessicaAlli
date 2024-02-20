b55ee58dddeb04952f43f27eb70d0ad47349d6b5
carra <allison.carr@bsu.edu>
2024-02-19 19:19:20 -0500

    Comments were added to class ArticleName for further comprehension of the actions completed by the class. AC

--- src/main/java/edu/bsu/cs/ArticleName.java
+++ src/main/java/edu/bsu/cs/ArticleName.java
@@ -3,11 +3,14 @@ package edu.bsu.cs;
 import java.util.Scanner;
 
 public class ArticleName {
+    //Creates an instance of the Scanner class
     public static Scanner console = new Scanner(System.in);
 
     public String ConfirmName() {
+        //Allows the user to input an article title
         System.out.println("Enter article name: ");
         String input = console.nextLine();
+        //If the input is empty, an error is returned
         if (input.isEmpty()) {
             System.err.println("No name entered");
         }
